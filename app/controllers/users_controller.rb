583de9fe436c21164d1e5bfadd641adad7986034
kyle macpherson <kaimac2010@gmail.com>
2015-11-02 16:18:00 +0000

    user models tests passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,14 @@ class UsersController < ApplicationController
 
   def create
     hash = JSON.parse(request.body.read)
-    user = User.new(email:(hash['email']),name:(hash['name']))
+    user = User.new(email:(hash['email']),name:(hash['name']),password:(hash['password']),password_confirmation:(hash['password_confirmation']))
     if user.save
       render json: user
     else
       render json: user.errors
     end
   end
- 
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates_presence_of :name, :email, :password_confirmation
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 5 }
+  validates :name, length: { minimum: 2}
 end
