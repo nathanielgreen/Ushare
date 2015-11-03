478e9b8082d2f270ca34a08a50b27dce34bf0895
zsid <zhivko.siderov@gmail.com>
2015-11-03 12:35:33 +0000

    refactoring done

--- spec/api/session_spec.rb
+++ spec/api/session_spec.rb
@@ -1,29 +1,23 @@
 require 'rails_helper'
-describe "users" do
 
-  def app
-    Rails.application
-  end
+describe "Sessions" do
+
+  # def app
+  #   Rails.application
+  # end
 
   context 'post request to /sessions' do
 
+    let!(:user) { create :user }
+
     context 'when password is correct' do
 
+      before { session_sign_in }
       it 'can start a new session' do
-        user = User.create(email: 'test@test.com', name: 'test boy', password: 'testing1', password_confirmation: 'testing1')
-        post '/sessions', {
-          email: 'test@test.com',
-          password: 'testing1',
-        }.to_json, {"CONTENT_TYPE" => 'application/json'}
         expect(last_response.status).to eq(201)
       end
 
       it 'returns authorisation key' do
-        user = User.create(email: 'test@test.com', name: 'test boy', password: 'testing1', password_confirmation: 'testing1')
-        post '/sessions', {
-          email: 'test@test.com',
-          password: 'testing1',
-        }.to_json, {"CONTENT_TYPE" => 'application/json'}
         json = JSON.parse(last_response.body)
         expect(json['auth_key']).not_to be(nil)
       end
@@ -32,31 +26,18 @@ describe "users" do
 
     context 'when password is wrong' do
 
+      before { session_wrong_sign_in }
+
       it 'returns 401 status code' do
-        user = User.create(email: 'test@test.com', name: 'test boy', password: 'testing1', password_confirmation: 'testing1')
-        post '/sessions', {
-          email: 'test@test.com',
-          password: 'incorrect',
-        }.to_json, {"CONTENT_TYPE" => 'application/json'}
         expect(last_response.status).to eq(401)
       end
 
       it 'renders error message' do
-        user = User.create(email: 'test@test.com', name: 'test boy', password: 'testing1', password_confirmation: 'testing1')
-        post '/sessions', {
-          email: 'test@test.com',
-          password: 'incorrect',
-        }.to_json, {"CONTENT_TYPE" => 'application/json'}
         json = JSON.parse(last_response.body)
         expect(json['messages']).to eq("Invalid Email or Password")
       end
 
       it 'does not return authorisation key' do
-        user = User.create(email: 'test@test.com', name: 'test boy', password: 'testing1', password_confirmation: 'testing1')
-        post '/sessions', {
-          email: 'test@test.com',
-          password: 'incorrect',
-        }.to_json, {"CONTENT_TYPE" => 'application/json'}
         json = JSON.parse(last_response.body)
         expect(json['auth_key']).to be(nil)
       end
