72a4c0710f7d077c647ad96da0436820558b210d
kyle macpherson <kaimac2010@gmail.com>
2015-11-10 10:56:09 +0000

    allowed delete requests

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module UberShare
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-          resource '*', :headers => :any, :methods => [:get, :post, :options, :put]
+          resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
