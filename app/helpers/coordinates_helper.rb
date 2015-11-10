4e34ebe4d15a5bf6b8b9297af8d13e20cc69b0e7
kyle macpherson <kaimac2010@gmail.com>
2015-11-10 15:45:06 +0000

    1

--- app/helpers/coordinates_helper.rb
+++ app/helpers/coordinates_helper.rb
@@ -1,12 +1,9 @@
 module CoordinatesHelper
 
-  def show_price(coordinate)
-    request = 'https://api.uber.com/v1/estimates/price?server_token=' + ENV['UBER_API']
-    final = request + '&start_latitude=' + coordinate.lat + '&start_longitude=' + coordinate.long + '&end_longitude=' + coordinate.long_end + '&end_latitude=' + coordinate.lat_end
-    HTTParty.get(final)
-  end
-
-  def all_coordinates?(coordinate)
-  end
+  # def show_price(coordinate)
+  #   request = 'https://api.uber.com/v1/estimates/price?server_token=' + ENV['UBER_API']
+  #   final = request + '&start_latitude=' + coordinate.lat + '&start_longitude=' + coordinate.long + '&end_longitude=' + coordinate.long_end + '&end_latitude=' + coordinate.lat_end
+  #   HTTParty.get(final)
+  # end
 
 end
